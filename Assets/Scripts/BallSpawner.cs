59b5c6c2dcadfcabb50baccc700f6dd3fbd6d4c0
Matthew Blum <45472455+mblum6180@users.noreply.github.com>
2023-05-10 16:47:52 -0400

    spawn update

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class BallSpawner : MonoBehaviour
 {
     public GameObject ballPrefab;
-    public float spawnYPosition = 4f;
+    public float spawnYPosition = -1.9f;
+    public float spawnXPosition = -0.9f;
     public float minXPosition = -4f;
     public float maxXPosition = 4f;
+    public float spawnZPosition = 10f;
+
+    public float minImpulseForce = 0.3f; // Minimum impulse force
+    public float maxImpulseForce = 0.7f; // Maximum impulse force
+
+    public float minImpulseAngleDegrees = 90f; // Minimum impulse angle
+    public float maxImpulseAngleDegrees = 130f; // Maximum impulse angle
 
     void Update()
     {
@@ -20,7 +28,24 @@ public class BallSpawner : MonoBehaviour
     void SpawnBall()
     {
         float randomX = Random.Range(minXPosition, maxXPosition);
-        Vector2 spawnPosition = new Vector2(randomX, spawnYPosition);
-        Instantiate(ballPrefab, spawnPosition, Quaternion.identity);
+        Vector3 spawnPosition = new Vector3(spawnXPosition, spawnYPosition, spawnZPosition);
+        
+        // Instantiate the ball and get its Rigidbody2D component
+        GameObject ball = Instantiate(ballPrefab, spawnPosition, Quaternion.identity);
+        Rigidbody2D ballRigidbody = ball.GetComponent<Rigidbody2D>();
+        
+        // If the ball has a Rigidbody2D, apply an impulse force
+        if (ballRigidbody != null)
+        {
+            // Generate a random impulse force and angle
+            float impulseForce = Random.Range(minImpulseForce, maxImpulseForce);
+            float impulseAngleDegrees = Random.Range(minImpulseAngleDegrees, maxImpulseAngleDegrees);
+            
+            // Convert the impulse angle to radians and create a direction vector
+            float impulseAngleRadians = impulseAngleDegrees * Mathf.Deg2Rad;
+            Vector2 impulseDirection = new Vector2(Mathf.Cos(impulseAngleRadians), Mathf.Sin(impulseAngleRadians));
+
+            ballRigidbody.AddForce(impulseDirection * impulseForce, ForceMode2D.Impulse);
+        }
     }
 }
